f05ac69a2b5bfd18998be5d6c9a8aea9fbc2140d
Randy Mungai <134689399+itsrandyM@users.noreply.github.com>
2025-09-17 11:53:50 +0200

    Update vite.config.ts
    Added sever host

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
-    allowedHosts: ["19be35d9-f986-484c-b5b3-1a946d1fa02a-00-19s615yvamoyh.picard.replit.dev"],
+    allowedHosts: "19be35d9-f986-484c-b5b3-1a946d1fa02a-00-19s615yvamoyh.picard.replit.dev",
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
